1cd787795509d08d37c7cd73fe4043a116b60b72
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2020-04-27 10:22:27 +0200

    Experimenting with user interaction

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@
 
 // Consider  using a struct or enum variant for each state, with each transition function being a method defined on it that takes self and returns Self.
 
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
 use std::ops::Deref;
 
 fn main() {
     // Initialize state machine
     let mut application = StateMachine {
-        secret_key: "Swordfish".to_string(),
+        secret_key: String::from("Swordfish"),
+        terminal: Term::stdout(),
     };
     let mut app_state = State(StateMachine::init);
 
@@ -46,21 +49,60 @@ impl Deref for State {
 #[derive(Debug)]
 struct StateMachine {
     secret_key: String,
+    terminal: Term,
 }
 
 impl StateMachine {
     fn init(&mut self) -> State {
-        println!("Start!");
-        State(Self::compute)
+        println!("Initializing!");
+        // self.terminal.write_line("Initializing!");
+        State(Self::home)
     }
 
-    fn compute(&mut self) -> State {
-        println!("Computing");
+    fn home(&mut self) -> State {
+        self.terminal.clear_screen();
+        println!("Welcome!\n");
+
+        let options = &[
+            "Send",
+            "Receive",
+            "End",
+        ];
+
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Watcha wanna do?")
+            .default(0)
+            .items(&options[..])
+            .interact()
+            .unwrap();
+        
+        self.terminal.clear_screen();
+
+        match selection {
+            0 => return State(Self::send),
+            1 => return State(Self::receive),
+            2 => return State(Self::end),
+            _ => return State(Self::exit),
+        }
+
+        State(Self::send)
+    }
+
+    fn send(&mut self) -> State {
+        println!("Sending!");
+        // self.terminal.write_line("Sending!");
+        State(Self::end)
+    }
+
+    fn receive(&mut self) -> State {
+        println!("Receiving!");
+        // self.terminal.write_line("Receiving!");
         State(Self::end)
     }
 
     fn end(&mut self) -> State {
         println!("Bye!");
+        // self.terminal.write_line("Bye!");
         State(Self::exit)
     }
 
