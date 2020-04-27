5bfbb808485941e1da10986bee006f96ec7e1de6
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2020-04-27 21:08:16 +0200

    Making state machine interface nicer

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-// https://hoverbear.org/blog/rust-state-machine-pattern/
-// https://www.reddit.com/r/rust/comments/ft1hqh/state_machines_in_rust/
-// http://cliffle.com/blog/rust-typestate/
-// https://www.reddit.com/r/rust/comments/57ccds/pretty_state_machine_patterns_in_rust/d8rhwq4/
-// https://dev.to/mindflavor/lets-build-zork-using-rust-1opm
-
-
-// Consider  using a struct or enum variant for each state, with each transition function being a method defined on it that takes self and returns Self.
-
 use console::Term;
 use dialoguer::{theme::ColorfulTheme, Select};
+use indicatif::{ProgressBar, ProgressStyle};
 use std::ops::Deref;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     // Initialize state machine
     let mut application = StateMachine {
         secret_key: String::from("Swordfish"),
         terminal: Term::stdout(),
+        state: State(StateMachine::init),
     };
-    let mut app_state = State(StateMachine::init);
 
     // Run state machine
-    while app_state != State(StateMachine::exit) {
-        app_state = app_state(&mut application);
+    while application.state != State(StateMachine::exit) {
+        application.run();
     }
 }
 
@@ -46,67 +40,81 @@ impl Deref for State {
 }
 
 
-#[derive(Debug)]
 struct StateMachine {
     secret_key: String,
+
+    state: State,
     terminal: Term,
 }
 
 impl StateMachine {
-    fn init(&mut self) -> State {
-        println!("Initializing!");
-        // self.terminal.write_line("Initializing!");
-        State(Self::home)
+    fn run(&mut self) {
+        self.state = (self.state)(self);
     }
 
+    // #####################
+    // State implementations
+    // #####################
+    
+    fn end(&mut self) -> State {
+        self.terminal.clear_screen();
+        State(Self::exit)
+    }
+    
+    fn exit(&mut self) -> State {
+        State(Self::exit)
+    }
+    
     fn home(&mut self) -> State {
         self.terminal.clear_screen();
-        println!("Welcome!\n");
-
+        
         let options = &[
             "Send",
             "Receive",
             "End",
-        ];
-
+            ];
+            
         let selection = Select::with_theme(&ColorfulTheme::default())
-            .with_prompt("Watcha wanna do?")
-            .default(0)
-            .items(&options[..])
-            .interact()
-            .unwrap();
+        .with_prompt("Choose an option:")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
         
         self.terminal.clear_screen();
-
+        
+        // TODO: Find better way for doing this. Matching against enums or something instead would be nice. 
         match selection {
-            0 => return State(Self::send),
-            1 => return State(Self::receive),
-            2 => return State(Self::end),
-            _ => return State(Self::exit),
+            0 => State(Self::send),
+            1 => State(Self::receive),
+            2 => State(Self::end),
+            _ => State(Self::exit),
         }
-
-        State(Self::send)
     }
-
-    fn send(&mut self) -> State {
-        println!("Sending!");
-        // self.terminal.write_line("Sending!");
-        State(Self::end)
+    
+    fn init(&mut self) -> State {
+        self.terminal.clear_screen();
+        println!("Initializing!");
+        State(Self::home)
     }
-
+    
     fn receive(&mut self) -> State {
         println!("Receiving!");
-        // self.terminal.write_line("Receiving!");
         State(Self::end)
     }
-
-    fn end(&mut self) -> State {
-        println!("Bye!");
-        // self.terminal.write_line("Bye!");
-        State(Self::exit)
-    }
-
-    fn exit(&mut self) -> State {
-        State(Self::exit)
+    
+    fn send(&mut self) -> State {
+        println!("Sending!");
+        
+        let pb = ProgressBar::new(1024);
+        
+        for _ in 0..1024 {
+            pb.inc(1);
+            thread::sleep(Duration::from_millis(5));
+        }
+        
+        pb.finish_with_message("done");
+        
+        State(Self::end)
     }
 }
